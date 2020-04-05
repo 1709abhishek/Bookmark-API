ce9fed2dbc1889f4fe3078ed039b04a689cb3043
Abhishek Jain <1709abhishek@gmail.com>
2020-04-05 15:52:42 +0530

    setup directory structure and server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const mongoose = require('mongoose');
+const db = require('./config/mongoose');
+
+
+const bodyParser = require('body-parser');
+
+// body parser for req.body.params
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+//requiring the routes
+app.use('/', require('./api/routes'));
+
+//running the express server
+app.listen(port, function(err){
+    if(err){
+        console.log('Error in running the server: ',err);
+    }
+     console.log(`server is running on port: ${port}`);
+});
